d70aa282baec670aaba8cd16ae63d9a3dd4d0db1
Adryan Vilela <122546032+AdryanVilela@users.noreply.github.com>
2023-03-27 15:02:26 -0300

    finalizado o login

--- home.php
+++ home.php
@@ -1,3 +1,7 @@
-<?php 
-session_start()
+<?php  
+session_start();
+$usuario="usuario";
+$senha="senha";
+// finalizado
+
 ?>
--- login.php
+++ login.php
@@ -2,4 +2,14 @@
 //trabalhando no modelo de login 
 
 // finalizando sistema de login
+
+$nome="eduarda";
+$senha="senha";
+
+if($senha="senha") echo"liberado"
+
+//teste
+
+
+
 ?>
